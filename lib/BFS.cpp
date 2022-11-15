bce47d2fe8a92f30bcbf546e1a8f8e654381887c
Edwin Ing <47252149+EdwinIngJ@users.noreply.github.com>
2022-11-15 22:16:38 +0000

    Implemented BFS to count number of components

--- lib/BFS.cpp
+++ lib/BFS.cpp
@@ -1,13 +1,43 @@
 #include "BFS.hpp"
+#include <queue>
 
 BFS::BFS(){}
 
-std::vector<int> BFS::performBFS(Graph g){
+std::vector<int> BFS::performBFS(Graph* g) {
     return std::vector<int>();
 }
 
-int BFS::countComponents(Graph g){
-    return -1;
+int BFS::countComponents(Graph* g) {
+    graph_ = g;
+    int numVertices = g->getNumVertices();
+    visited_vertices_ = std::vector<bool>(numVertices, false);
+
+    int numComponents = 0;
+    for (int v = 0; v < numVertices; v++) {
+        if (visited_vertices_[v] == false) {
+            BFS_helper(v);
+            numComponents += 1;
+        }
+    }
+
+    return numComponents;
 }
 
-void BFS::BFS_helper(Graph g, int vertex){}
+void BFS::BFS_helper(int vertex) {
+    std::queue<int> q;
+    visited_vertices_[vertex] = true;
+
+    q.push(vertex);
+
+    while (!q.empty()) {
+        int v = q.front();
+        q.pop();
+        for (int neighbor: graph_->adjacent(v)) {
+            if (visited_vertices_[neighbor] == false) {
+                visited_vertices_[neighbor] = true;
+                q.push(neighbor);
+            }
+        }
+    }
+    
+}
