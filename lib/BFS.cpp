199ff655f9ee646e5f8b98a68c996c611c6763e2
Edwin Ing <47252149+EdwinIngJ@users.noreply.github.com>
2022-12-02 07:01:01 +0000

    Modified BFS to test for correct traversal

--- lib/BFS.cpp
+++ lib/BFS.cpp
@@ -4,37 +4,6 @@
 
 BFS::BFS(){}
 
-std::vector<int> BFS::performBFS(Graph* g) {
-    graph_ = g;
-    int numVertices = g->getNumVertices();
-    visited_vertices_ = std::vector<bool>(numVertices, false);
-    visited_edges_ = std::vector<std::vector<int>>(numVertices, std::vector<int>(numVertices, -1));
-
-    for (int v = 0; v < numVertices; v++) {
-        if (visited_vertices_[v] == false) {
-            BFS_helper(v);
-        }
-    }
-
-    std::vector<int> edges;
-
-    int count = 0;
-    for (auto row: visited_edges_) {
-        for (auto elem: row) {
-            if (elem != -1) {
-                // std::cout << elem << std::endl;
-                edges.push_back(elem);
-                if (elem == 0) {
-                    count++;
-                }
-            }
-        }
-    }
-    std::cout << "Count: " << count;
-    std::cout << edges.size();
-    return edges;
-}
-
 int BFS::countComponents(Graph* g) {
     graph_ = g;
     int numVertices = g->getNumVertices();
@@ -62,24 +31,47 @@ void BFS::BFS_helper(int vertex) {
         int v = q.front();
         q.pop();
         for (int neighbor: graph_->adjacent(v)) {
-            int larger = -1;
-            int smaller = -1;
-            if (v < neighbor) {
-                larger = neighbor;
-                smaller = v;
-            } else {
-                larger = v;
-                smaller = neighbor;
-            }
-
             if (visited_vertices_[neighbor] == false) {
-                visited_edges_[smaller][larger] = 1;
                 visited_vertices_[neighbor] = true;
                 q.push(neighbor);
-            } else if (visited_edges_[smaller][larger] == -1) {
-                visited_edges_[smaller][larger] = 0;
             }
         }
     }
     
+}
+
+std::unordered_map<int, double> BFS::performBFS(Graph* g) {
+    graph_ = g;
+    int numVertices = g->getNumVertices();
+    visited_vertices_ = std::vector<bool>(numVertices, false);
+    std::unordered_map<int, double> distances;
+
+
+    for (int v = 0; v < numVertices; v++) {
+        if (visited_vertices_[v] == false) {
+            distances[v] = 0;
+            BFS_helper(v, distances);
+        }
+    }
+
+    return distances;
+}
+
+void BFS::BFS_helper(int vertex, std::unordered_map<int, double>& distances) {
+    std::queue<int> q;
+    visited_vertices_[vertex] = true;
+
+    q.push(vertex);
+
+    while (!q.empty()) {
+        int v = q.front();
+        q.pop();
+        for (int neighbor: graph_->adjacent(v)) {
+            if (visited_vertices_[neighbor] == false) {
+                visited_vertices_[neighbor] = true;
+                q.push(neighbor);
+                distances[neighbor] = distances[v] + graph_->edgeValue(graph_->indexOfAdjacentEdge(v, neighbor));
+            }
+        }
+    }
 }
